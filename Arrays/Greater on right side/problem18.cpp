da5d11ac11caffea745e8fc2b238f4a7e4cb0608
vedantmittal <mittalvedant5@gmail.com>
2020-09-09 00:19:57 +0530

    ADD PROBLEM: Greater on right side

--- Arrays/Greater on right side/problem18.cpp
+++ Arrays/Greater on right side/problem18.cpp
@@ -1,24 +1,38 @@
 #include<iostream>
 #include<map>
 using namespace std;
-
 int main(){
-
-    int n;
-    cin>>n;
-
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    int a;
+    cin>>a;
+    while (a--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        map<int,int> val;
+        map<int,int>::reverse_iterator it;
+        for(int i=0;i<n;i++){
+            val[arr[i]] = i;
+        }
+        it = val.rbegin();
+        for(int i=0;i<n,it!=val.rend();i++){
+            if(i < it->second){
+                arr[i] = it->first;
+            }
+            else if(i == n-1){
+                arr[i] = -1;
+            }
+            else{
+                ++it;
+                i = i-1;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {   
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
     }
-
-    map<int,int> val;
-    for(int i=0;i<n;i++){
-        val[arr[i]]++;
-    }
-
-    for(auto x : val){
-        cout<<x.first<<"  "<<x.second<<endl;
-    }
-
 }
