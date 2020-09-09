2861e8e2d63d7d7c40213bf548616a14de1ad742
vedantmittal <mittalvedant5@gmail.com>
2020-09-09 23:25:48 +0530

    ADD PROBLEM: Balanced Array

---
+++ Arrays/Balanced Array/problem27.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a;
+    while(a--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int temp = n/2;
+        int temp1=0,temp2=0,val;
+        for(int i=0;i<n;i++){
+            if(i<temp){
+                temp1 += arr[i];
+            }
+            else{
+                temp2 += arr[i];
+            }
+        }
+        val = abs(temp1 - temp2);
+        cout<<val<<endl;
+    }
+}
