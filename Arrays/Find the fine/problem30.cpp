33e93f63054993ac2c4365575441496da55000eb
vedantmittal <mittalvedant5@gmail.com>
2020-09-12 12:05:56 +0530

    ADD Problem: Find the fine

--- Arrays/Find the fine/problem30.cpp
+++ Arrays/Find the fine/problem30.cpp
@@ -3,6 +3,34 @@ using namespace std;
 
 int main(){
 
+    int a;
+    cin>>a;
+    while(a--){
+        int n,k;
+        cin>>n>>k;
+        int arr[n],fn[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        for(int i=0;i<n;i++){
+            cin>>fn[i];
+        }
+        int temp=0;
+        for(int i=0;i<n;i++){
+            if(k%2 == 0){
+                if(arr[i]%2!=0){
+                    temp = temp + fn[i];
+                }
+            }
+            else{
+                if(arr[i]%2==0){
+                    temp = temp + fn[i];
+                }
+            }
+        }
+        cout<<temp<<endl;
+        
+        
+    }
 
-    
 }
