1992f70bdc676bec1324cee926df7ef2fdc6525e
vedantmittal <mittalvedant5@gmail.com>
2020-09-13 17:27:08 +0530

    ADD Problem: Efficient Vineet

---
+++ Arrays/Efficient Vineet/problem36.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    float arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    int j=0,k=n-1,count=0;
+    while(j<=k){
+        float val = arr[j]+arr[k];
+        if(val<=3.0){
+            cout<<"hi";
+
+            ++count;
+            j++;
+            k--;
+        }
+        else{
+            ++count;
+            k--;
+        }
+    }
+    cout<<count<<endl;
+}
