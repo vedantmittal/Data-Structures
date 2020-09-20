672fa4cb6553375c09d3d298377075f47c51203c
vedantmittal <mittalvedant5@gmail.com>
2020-09-20 13:50:38 +0530

    ADD PROBLEM: /Find n by k th node in Linked list

---
+++ Linked List/Find n by k th node in Linked list/problem10.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fractional_node(struct Node* head,int k);
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        Node* ptr,*start = NULL,*ptr1;
+        int n,i;
+        cin>>n;
+        
+        for(i=0;i<n;i++)
+        {
+            int value;
+            cin>>value;
+            
+            ptr=new Node(value);
+            
+            if(start==NULL)
+            {
+                start=ptr;
+                ptr1=ptr;
+            }
+            else
+            {
+                ptr1->next=ptr;
+                ptr1=ptr1->next;
+            }
+        }
+        ptr1->next=NULL;
+        int k;
+        cin>>k;
+        int p = fractional_node(start,k);
+        cout<<p<<endl;
+    }
+}
+int fractional_node(struct Node *head, int k)
+{
+    Node *temp = head;
+    int count =0;
+    while(temp!=NULL){
+        temp = temp->next;
+        ++count;
+    }
+    int element = count / k;
+    temp = head;
+    if(count%2 == 0){
+        element = element -1;
+    }
+    while(element --){
+        temp = temp->next;
+    }
+    return temp->data;
+}
