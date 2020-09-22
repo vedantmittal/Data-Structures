d2888daf3616225478028c03eed08b44015dfdd8
vedantmittal <mittalvedant5@gmail.com>
2020-09-22 14:02:04 +0530

    ADD PROBLEM: Rotate doubly Linked List

---
+++ Linked List/Rotate doubly Linked List/problem13.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+  int data;
+  struct node*next,*prev;
+  
+  node(int x){
+      data = x;
+      next = NULL;
+      prev = NULL;
+  }
+  
+};
+struct node* update(struct node*start,int p);
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,p;
+        cin>>n>>p;
+        struct node*start = NULL;
+        struct node* cur = NULL;
+        struct node* ptr = NULL;
+        
+        for(int i=0;i<n;i++)
+        {
+            int a;
+            cin>>a;
+            ptr=new node(a);
+            ptr->data=a;
+            ptr->next=NULL;
+            ptr->prev=NULL;
+            if(start==NULL)
+            {
+                start=ptr;
+                cur=ptr;
+            }
+            else
+            {
+                cur->next=ptr;
+                ptr->prev=cur;
+                cur=ptr;
+            }
+        }
+        struct node*str=update(start,p);
+        while(str!=NULL)
+        {
+            cout<<str->data<<" ";
+            str=str->next;
+        }
+        cout<<endl;  
+    }
+}
+
+struct node*update(struct node*start,int p)
+{
+    struct node *temp = start;
+    struct node *val = NULL;
+    struct node *headnew = NULL;
+    while(p--){
+        temp = temp->next;
+    }
+    temp->prev = NULL;
+    headnew = temp;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    while(start!=headnew){
+        temp->next = start;
+        start->prev = temp;
+        start = start->next;
+        temp = temp->next;
+    }
+    temp->next = NULL;
+    return headnew;
+}
