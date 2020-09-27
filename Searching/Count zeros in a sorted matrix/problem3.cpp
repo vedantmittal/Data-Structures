63a554ccd6c3a8e829b3b4013431a8a81ea882bb
vedantmittal <mittalvedant5@gmail.com>
2020-09-27 13:07:02 +0530

    ADD PROBLEM: Count zeros in a sorted matrix

---
+++ Searching/Count zeros in a sorted matrix/problem3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+#define MAX 1001
+int countZeros(int A[MAX][MAX],int N);
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int A[MAX][MAX];
+		for(int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+		cin>>A[i][j];
+		cout<<countZeros(A,n)<<endl;
+	}
+	return 0;
+}
+
+int countZeros(int A[MAX][MAX],int N)
+{
+    int count = 0;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            if(A[i][j]==0){
+                ++count;
+            }
+        }
+    }
+	return count;
+}
