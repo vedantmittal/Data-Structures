82d1b531a16d1c8c18f8588566f31ffd4a853815
vedantmittal <mittalvedant5@gmail.com>
2020-10-02 00:20:03 +0530

    ADD PROBLEM: Doubling the value

--- Searching/Doubling the value/problem6.cpp
+++ Searching/Doubling the value/problem6.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
 int main(){
     int a;
     cin>>a;
@@ -10,7 +11,6 @@ int main(){
             cin>>arr[i];
         }
         int temp = k;
-        int z;
         for(int i=0;i<n;i++){
             if(temp== arr[i]){
                 temp = 2*temp;
