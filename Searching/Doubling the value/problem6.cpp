e78935864a6ac01b6d37ac174149d3e247cc4944
vedantmittal <mittalvedant5@gmail.com>
2020-10-01 23:57:06 +0530

    ADD PROBLEM: Doubling the value

---
+++ Searching/Doubling the value/problem6.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+int main(){
+    int a;
+    cin>>a;
+    while(a--){
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int temp = k;
+        for(int i=0;i<n;i++){
+            if(temp== arr[i]){
+                temp = 2*temp;
+            }
+        }
+        return temp;
+    }
+}
