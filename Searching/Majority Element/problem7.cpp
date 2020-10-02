0d90c6ab39fa86a860d46bc6f71ec3d4af96b6af
vedantmittal <mittalvedant5@gmail.com>
2020-10-02 20:11:53 +0530

    ADD PROBLEM: Majority Element

--- Searching/Majority Element/problem7.cpp
+++ Searching/Majority Element/problem7.cpp
@@ -14,15 +14,16 @@ int main(){
         for(int i=0;i<n;i++){
             cin>>arr[i];
         }
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n;i++){
             val[arr[i]]++;
         }
         int temp=-1;
         for(auto it:val){
             if(it.second > n/2){
                 temp = it.first;
-                cout<<temp<<endl;
+                break;
             }
         }
+        cout<<temp<<endl;
     }
 }
