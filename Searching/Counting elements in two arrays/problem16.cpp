cfef012c5523de258b9390f24eee8797f1344974
vedantmittal <mittalvedant5@gmail.com>
2020-10-28 23:45:06 +0530

    ADD PROBLEM:Counting elements in two arrays

---
+++ Searching/Counting elements in two arrays/problem16.cpp
@@ -0,0 +1,51 @@
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+int binary_search(int arr[], int l, int h, int x);
+vector <int> countEleLessThanOrEqual(int arr1[], int arr2[], 
+                             int m, int n);
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m,n;
+        cin>>m>>n;
+        int arr1[m],arr2[n];
+        for(int i=0;i<m;i++)
+        cin>>arr1[i];
+        for(int j=0;j<n;j++)
+        cin>>arr2[j];
+        vector <int> res = countEleLessThanOrEqual(arr1, arr2, m, n);
+        for (int i = 0; i < res.size (); i++) cout << res[i] << " ";
+        
+        cout<<endl;
+    }
+    return 0;
+}
+
+vector<int> countEleLessThanOrEqual(int arr1[], int arr2[], 
+                             int m, int n)
+{
+    sort(arr2,arr2+n);
+    vector<int> val;
+    for(int i=0;i<m;i++){
+        int element = arr1[i];
+        int start = 0,end = n-1;
+        int count=-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(element >= arr2[mid]){
+                start = mid+1;
+                count = mid;
+            }
+            else if(element<arr2[mid]){
+                end = mid-1;
+            }
+        }
+        val.push_back(count+1);
+    }
+    return val;
+}
--- Sorting/Rank List/problem1.cpp
+++ Sorting/Rank List/problem1.cpp
@@ -1,56 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void swap1(int arr[], int i, int j){
-	int temp = arr[i];
-	arr[i] = arr[j];
-	arr[j] = temp;
+static map<string,int> val;
+int count =0;
+
+void parket(string input){
+	val[input] = ++count;
+	cout<<"CAR PARKED AT POSITION:"<<count;
 }
 
-void swap2(string arr[], int i, int j){
-	string temp = arr[i];
-	arr[i] = arr[j];
-	arr[j] = temp;
+int search(string input){
+	if(val[input]!=val.end()){
+		cout<<"CAR POSITION:"<<val[input];
+	}
+	else{
+		cout<<"CAR DOES NOT EXISTS";
+	}
 }
+
+
 
 int main(){
+	val["MH04CC2"] = ++count;
+	val["MH04C2820"] = ++count;
+	val["MH04BB3232"] = ++count;
+	val["MH04CC2113"] = ++count;val["MH04CC2878"] = ++count;val["MH04BB8"] = ++count;val["MH04CC2888"] = ++count;val["MH04CC1313"] = ++count;val["MH04CC2222"] = ++count;
+	val["MH04C1201"] = ++count;
+	val["MH04CC555"] = ++count;
+	val["MH04C6565"] = ++count;
+	val["MH04A7"] = ++count;
+	
 	int n;
 	cin>>n;
-	string name[n];
-	int sch[n],marks[n];
-	for(int i=0;i<n;i++){
-		cin>>name[i];
-		cin>>sch[i]>>marks[i];
+	string input;
+	if(n==1){
+		cin.ignore();
+		getline(cin,input);
+		parket(input);
 	}
-	for(int i=0;i<n;i++){
-		for(int j=i+1;j<n;j++){
-			if(marks[j]>marks[i]){
-				swap1(marks,i,j);
-				swap1(sch,i,j);
-				swap2(name,i,j);
-			}
-		}
+	else if(n==2){
+		cin.ignore();
+		getline(cin,input);
+		search(input);
 	}
-	for(int i=0;i<n;i++){
-		for(int j=i+1;j<n;j++){
-			if(marks[i]==marks[j]){
-				if(name[i]>name[j]){
-					swap1(marks,i,j);
-					swap1(sch,i,j);
-					swap2(name,i,j);
-				}
-				else if(name[i]==name[j]){
-					if(sch[i]>sch[j]){
-						swap1(marks,i,j);
-						swap1(sch,i,j);
-						swap2(name,i,j);
-					}
-				}
-			}
-		}
+	else{
+		cout<<"INVALID INPUT";
 	}
-	for(int i=0;i<n;i++){
-		cout<<name[i]<<" "<<sch[i]<<" "<<marks[i]<<endl;
-	}	
 	
 }
